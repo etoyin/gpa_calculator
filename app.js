81bafd37d802a45f5857b5cb39c831b209f50f24
Toyin Adesina <toyinadesina60@gmail.com>
2020-08-15 10:04:54 +0100

    Another commit 3

--- app.js
+++ app.js
@@ -7,15 +7,14 @@ const app = express();
 const router = require("./modules/router");
 app.use(cors());
 
-
 app.use(bodyParser.json());
 
 app.use("/api/calculate", router);
 
 if(process.env.NODE_ENV === "production"){
-  app.use(express.static("client/build"));
+  app.use(express.static(path.join( __dirname, "public/build")));
   app.get("*", (req, res) => {
-    res.sendFile(path.jresolve(__dirname, "client", "build","index.html"));
+    res.sendFile(path.resolve(__dirname, "public", "build","index.html"));
   })
 }
 
---
+++ public/build/precache-manifest.d2aa4214cb9f806c5bfbd74eb3370315.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a1ef5c7cd321087300a3f4d443c2cab",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f266067c80346ab57df4",
+    "url": "/static/css/main.b156e9a2.chunk.css"
+  },
+  {
+    "revision": "22f30c80b50251bf9401",
+    "url": "/static/js/2.30b43afd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.30b43afd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f266067c80346ab57df4",
+    "url": "/static/js/main.5531cc09.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
